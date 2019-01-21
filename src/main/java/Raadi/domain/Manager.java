3f8810db3cdefe91e986edc5f648d6dd526025b9
Farah Nedjadi <farah.nedjadi@epita.fr>
2019-01-21 15:55:59 +0100

    Retro index done in Manager

--- src/main/java/Raadi/domain/Manager.java
+++ src/main/java/Raadi/domain/Manager.java
@@ -2,6 +2,8 @@ package Raadi.domain;
 
 import Raadi.domain.model.DocumentClean;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
@@ -11,5 +13,18 @@ public class Manager
     HashSet<String> linksDone;
     List<DocumentClean> documents;
 
+    HashMap<String, ArrayList<DocumentClean>> retroIndex;
+
+    public void fillRetroIndex() {
+        for (DocumentClean dc : documents) {
+             for (String key : dc.getVector().keySet()) {
+                 ArrayList<DocumentClean> value = new ArrayList<>();
+                 if (retroIndex.containsKey(key))
+                     value = retroIndex.get(key);
+                 value.add(dc);
+                 retroIndex.put(key, value);
+             }
+        }
+    }
 
 }
