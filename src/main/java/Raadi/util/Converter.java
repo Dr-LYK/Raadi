57a814e8bb3b2a8f16b33374ea7c6c67dc3aeddf
Vivian Chaizemartin <vivian.chaizemartin@epita.fr>
2019-01-21 17:02:20 +0100

    [UTIL][CONVERTER] CSV file Synonyms converter implemented

--- src/main/java/Raadi/util/Converter.java
+++ src/main/java/Raadi/util/Converter.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Converter {
 
-    public static HashSet<String> StopWordsJsonToHashSet(String path) throws IOException {
-
+    public static HashSet<String> StopWordsJsonToHashSet() throws IOException {
 
         byte[] encoded = Files.readAllBytes(Paths.get(System.getProperty("user.dir") + "/src/main/java/Raadi/util/StopWords.json"));
         String stopWordsString = new String(encoded, Charset.defaultCharset());
@@ -21,5 +21,28 @@ public class Converter {
             items[i] = items[i].substring(1, items[i].length() - 1);
 
         return new HashSet<>(Arrays.asList(items));
+    }
+
+    public static HashMap<String, String> SynonymsCSVToHashMap() throws IOException {
+
+        HashMap<String, String> synonymsHashMap = new HashMap<>();
+
+        String fileName = System.getProperty("user.dir") + "/src/main/java/Raadi/util/Synonyms.csv";
+        List<String> list = new ArrayList<>();
+
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+            list = stream.collect(Collectors.toList());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (String str : list) {
+            String[] items = str.split(",");
+            for (int i = 1; i < items.length; i++)
+                synonymsHashMap.put(items[i], items[0]);
+        }
+        
+        return synonymsHashMap;
     }
 }
