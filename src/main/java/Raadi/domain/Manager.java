66ff8bd100e1b308561a7ff85e89aa8776650d69
Vivian Chaizemartin <vivian.chaizemartin@epita.fr>
2019-01-21 15:52:35 +0100

    [DOMAIN][MANAGER] crawl and cleanup functions implemented

--- src/main/java/Raadi/domain/Manager.java
+++ src/main/java/Raadi/domain/Manager.java
@@ -1,15 +1,48 @@
 package Raadi.domain;
 
 import Raadi.domain.model.DocumentClean;
+import Raadi.domain.model.DocumentRaw;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
 public class Manager
 {
-    HashSet<String> linksTodo;
-    HashSet<String> linksDone;
-    List<DocumentClean> documents;
+    private HashSet<String> linksTodo;
+    private HashSet<String> linksDone;
+    private ArrayList<DocumentRaw> documentRawList;
+    private ArrayList<DocumentClean> documentCleanList;
 
+    private Manager() {
+        this.linksTodo = new HashSet<>();
+        this.linksDone = new HashSet<>();
+        this.documentRawList = new ArrayList<>();
+        this.documentCleanList = new ArrayList<>();
+    }
 
+    private static class ManagerHolder {
+        private final static Manager instance = new Manager();
+    }
+
+    public static Manager getInstance() {
+        return ManagerHolder.instance;
+    }
+
+    private void crawl() {
+        for (String url : linksTodo) {
+            if (!linksDone.contains(url)) {
+                documentRawList.add(Crawler.crawl(url));
+                linksTodo.remove(url);
+                linksDone.add(url);
+            }
+        }
+    }
+
+    private void cleanup() {
+        for (DocumentRaw documentRaw : documentRawList) {
+            documentCleanList.add(CleanUp.cleanup(documentRaw));
+            documentRawList.remove(documentRaw);
+        }
+    }
 }
