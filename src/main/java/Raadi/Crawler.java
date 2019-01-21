5d76897bfda72d6cddc7c92c5929bbf35400e5da
Farah Nedjadi <farah.nedjadi@epita.fr>
2019-01-21 14:31:15 +0100

    Crawler get content + childUrl

--- src/main/java/Raadi/Crawler.java
+++ src/main/java/Raadi/Crawler.java
@@ -1,11 +1,38 @@
 package Raadi;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+
 public final class Crawler
 {
     public static RaadiDocument crawl(String URL)
     {
         RaadiDocument raadiDocument = new RaadiDocument();
-        // TODO
+
+        try {
+            // Get the content of the page
+            Document doc = Jsoup.connect(URL).get();
+            Element content = doc.body();
+
+            // Get all the url linked in the page
+            Elements links = doc.select("a[href]");
+            ArrayList<String> childUrl = new ArrayList<>();
+            for (Element link : links) {
+                childUrl.add(link.attr("abs:href"));
+            }
+
+            // raadiDocument.content = body;
+            // raadiDocument.url = URL
+            // raadiDocument.childrenUrl = childUrl
+
+        } catch (Exception ex) {
+            System.err.print(ex);
+        }
+
         return raadiDocument;
     };
 }
