091431d5a5729f1414d8e8e86b71d6041fcf38bd
Vivian Chaizemartin <vivian.chaizemartin@epita.fr>
2019-01-21 15:09:14 +0100

    [MODEL][DOCUMENT] DocumentClean documentation done.

--- src/main/java/Raadi/domain/model/DocumentRaw.java
+++ src/main/java/Raadi/domain/model/DocumentRaw.java
@@ -4,43 +4,81 @@ import java.util.HashSet;
 
 public class DocumentRaw {
 
-    protected String content;
-    protected String URL;
-    protected String srcURL;
-    protected HashSet<String> childrenURL;
+    /**
+     * DocumentRaw attributes.
+     */
+    private String content;
+    private String URL;
+    private String srcURL;
+    private HashSet<String> childrenURL;
 
-    public DocumentRaw() {
-    }
-
+    /**
+     * DocumentRaw content getter.
+     * @return the document content.
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * DocumentRaw content setter.
+     * @param content is the document content.
+     */
     public void setContent(String content) {
         this.content = content;
     }
 
+    /**
+     * DocumentRaw URL getter.
+     * @return the document URL.
+     */
     public String getURL() {
         return URL;
     }
 
+    /**
+     * DocumentRaw URL setter.
+     * @param URL is the document URL.
+     */
     public void setURL(String URL) {
         this.URL = URL;
     }
 
+    /**
+     * DocumentRaw source URL getter.
+     * @return the document URL where it comes from.
+     */
     public String getSrcURL() {
         return srcURL;
     }
 
+    /**
+     * DocumentRaw source URL setter.
+     * @param srcURL is the document URL where it comes from.
+     */
     public void setSrcURL(String srcURL) {
         this.srcURL = srcURL;
     }
 
+    /**
+     * DocumentRaw URL children getter.
+     * @return the document's list of URL children.
+     */
     public HashSet<String> getChildrenURL() {
         return childrenURL;
     }
 
+    /**
+     * DocumentRaw URL   setter
+     * @param childrenURL is list of URL children
+     */
     public void setChildrenURL(HashSet<String> childrenURL) {
         this.childrenURL = childrenURL;
+    }
+
+    /**
+     * DocumentRaw constructor.
+     */
+    public DocumentRaw() {
     }
 }
